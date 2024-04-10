a821bdeebe0f4e601dbd3c5f0e11036c3729c561
Jisun696 <istiakkashem35@gmail.com>
2024-04-10 06:32:04 +0600

    installed react-lottie package
    Completed 404 error page and used a animation from lottiFiles using the react-lottie package

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,11 +3,13 @@ import Root from './Root';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import Register from '../pages/register/Register';
+import ErrorPage from '../pages/error-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
