e0e24ab37bbf66b5431c8ffdea0da2ccc3d0d7e2
Jisun696 <istiakkashem35@gmail.com>
2024-04-12 01:30:44 +0600

    Added ScrollRestoration component in Root.jsx to restore scroll position after navigating

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { ScrollRestoration } from 'react-router-dom';
 
 const Root = () => {
   return (
     <>
+      <ScrollRestoration />
       <Header />
       <main>
         <Outlet />
