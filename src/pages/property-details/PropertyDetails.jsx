4c6af50069d49fa50c8cd0f78cf2d28f5ea61f79
Jisun696 <istiakkashem35@gmail.com>
2024-04-15 03:36:45 +0600

    modified json data & started building detail page

--- src/pages/property-details/PropertyDetails.jsx
+++ src/pages/property-details/PropertyDetails.jsx
@@ -11,10 +11,14 @@ const PropertyDetails = () => {
   console.log(property);
 
   return (
-    <div>
-      <h1>
-        PROPERTY : <strong>{property.estate_title}</strong>
-      </h1>
+    <div className="flex container mx-auto gap-5 pb-10">
+      <div className="w-1/2 rounded-lg overflow-hidden">
+        <img src={property.image} alt="" />
+      </div>
+      <div className="w-1/2">
+        <h2 className="text-neutral-900 text-opacity-80 text-lg font-medium mb-1 font-kufam">{property.segment_name}</h2>
+        <h1 className="text-neutral-900 text-4xl font-bold mb-4 font-kufam">{property.estate_title}</h1>
+      </div>
     </div>
   );
 };
