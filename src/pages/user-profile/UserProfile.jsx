49c2c2609be9b9e5effe57b284942081dc88bce8
Jisun696 <istiakkashem35@gmail.com>
2024-04-17 03:41:04 +0600

    added dynamic title for UserProfile page

--- src/pages/user-profile/UserProfile.jsx
+++ src/pages/user-profile/UserProfile.jsx
@@ -2,11 +2,15 @@ import React, { useContext } from 'react';
 import image from '../../assets/undraw_People_re_8spw.png';
 import { AuthContext } from '../../providers/AuthProvider';
 import defaultPlaceholder from '../../assets/Default_Placeholder.webp';
+import { Helmet } from 'react-helmet-async';
 
 const UserProfile = () => {
   const { user } = useContext(AuthContext);
   return (
     <div data-aos="fade-zoom-in" data-aos-duration="700" className="flex flex-col lg:flex-row gap-6 container mx-auto min-h-[calc(100vh-172px)] items-center justify-center ">
+      <Helmet>
+        <title>Homelocus | User Profile</title>
+      </Helmet>
       <div className="w-1/2 max-lg:w-full  px-6 sm:px-20 py-6 sm:pt-10">
         <div className="w-24 h-24 rounded-lg overflow-hidden">
           <img src={user.photoURL || defaultPlaceholder} referrerPolicy="no-referrer" />
